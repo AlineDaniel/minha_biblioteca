10dbf20953d3ae55a357518e33f7b4da0c5cd5ca
“AlineDaniel” <alinedaniel18@gmail.com>
2024-03-18 18:10:11 -0300

    first commit

---
+++ lib/pages/home/widgets/new_category/add_category.store.dart
@@ -0,0 +1,45 @@
+import 'dart:io';
+
+import 'package:get_it/get_it.dart';
+import 'package:mobx/mobx.dart';
+
+import '../../../../models/category.model.dart';
+import '../../../../repositories/category.repository.dart';
+
+// Include generated file
+part 'add_category.store.g.dart';
+
+// This is the class used by rest of your codebase
+class AddCategoryStore = AddCategoryStoreBase with _$AddCategoryStore;
+
+// The store-class
+abstract class AddCategoryStoreBase with Store {
+  @observable
+  File? imageCategory;
+
+  @observable
+  bool isLoading = false;
+
+  @action
+  void setImage(File image) => imageCategory = image;
+
+  @action
+  void removeImage() => imageCategory = null;
+
+  @action
+  Future<void> addCategory(String nameCategory) async {
+    isLoading = true;
+
+    final repository = GetIt.I.get<CategoryRepository>();
+
+    await repository.addNewCategory(
+      Category(
+        id: DateTime.now().millisecond.toString(),
+        name: nameCategory,
+        filePath: imageCategory != null ? imageCategory!.path : null,
+      ),
+    );
+
+    isLoading = false;
+  }
+}
